883829599233dbf9440799dd146a1ddbd28ffb08
huyhoang39 <fui.hq22@email.com>
2024-10-28 01:56:43 +0700

    empty respo

--- sdk/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './app-client';
-export * from './generated';
-export * from './generated/core/ApiRequestOptions';
--- src/app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }: { children: Readonly<React.ReactNode> }) {
-  return (
-    <div className="grid min-h-svh place-content-center">
-      <div>{children}</div>
-    </div>
-  );
-}
